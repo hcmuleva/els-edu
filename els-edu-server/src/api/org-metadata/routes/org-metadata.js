f9aee916334f9382296816ba35d7d631f5d80bae
hcmuleva <hcmuleva@gmail.com>
2025-12-19 15:07:18 +0530

    Org meta data deleted

--- els-edu-server/src/api/org-metadata/routes/org-metadata.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * org-metadata router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::org-metadata.org-metadata');
--- els-edu-server/src/api/org-metadata/services/org-metadata.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * org-metadata service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::org-metadata.org-metadata');
